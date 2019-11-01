be3548a70b52cb4665b8a34c18943aa0ae35f2b5
Zachary Swanson <zachary.j.swanson@gmail.com>
2019-11-01 16:51:40 -0700

    update schema with latest migrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2019_10_29_232236) do
 
   create_table "images", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.string "description"
+    t.string "name", null: false
+    t.string "url", null: false
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
